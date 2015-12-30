cae83691479084337d10b513cb6ddd5fb1962fa9
Stephan Cilliers <stephanus.cilliers@gmail.com>
2015-12-30 23:08:54 +0200

    Removed useless comments in main()

--- src/wdbm.py
+++ src/wdbm.py
@@ -89,16 +89,6 @@ def filtrate(text):
     return filtered
 
 def main():
-    # content = []
-    # database_name = "database.db"
-    # inputfile_name = "input.txt"
-    # with open(inputfile_name, "r") as inputf:
-    #     for line in inputf:
-    #         content.append(line)
-    # content = filtrate(" ".join(content))
-    # database = load(database_name)
-    # database = update(content, database)
-    # write(database_name, database)
     pass
 
 if __name__ == "__main__":
