345382ef26f6b71995267514dc79e860cf95d2c7
Stephan Cilliers <stephanus.cilliers@gmail.com>
2015-12-31 22:16:18 +0200

    Implemented argparse for command line arguments

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import praw
 import sys
 import time
@@ -5,28 +6,37 @@ import wdbm
 
 
 def main():
-    try:
-        target_sub = sys.argv[1]    # First arg is filename
-    except Exception as e:
-        target_sub = "all"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target",
+                        help="targeted subreddit",
+                        default="all")
+    parser.add_argument("-n", "--posts",
+                        help="number of posts to fetch",
+                        default=5, type=int)
+    args = parser.parse_args()
+
+    target_sub = args.target
+    submission_count = args.posts
+
     database_name = "db_{}.db".format(target_sub)
     database = {}
     submissions_scanned_f = "submissions_scanned.txt"
     database, submissions_scanned = wdbm.load(database_name,
                                               submissions_scanned_f)
+
     r = praw.Reddit(user_agent='''Documenting Reddit vocabulary.
                                 by /u/tookieewooper''')
     subreddit = r.get_subreddit(target_sub)
     try:
-        submissions = subreddit.get_top_from_day(limit=5)
-        print "Fetched submissions from {}.".format(target_sub)
+        submissions = subreddit.get_top_from_day(limit=submission_count)
+        print "Fetched {} submissions from {}.".format(submission_count,
+                                                       target_sub)
     except Exception as e:
         print "Couldn't fetch submissions from {}.".format(target_sub)
 
     for submission in submissions:
         if submission.id not in submissions_scanned:
             flat_comments = praw.helpers.flatten_tree(submission.comments)
-            print "Flattened comments."
             already_done = set()
             for comment in flat_comments:
                 if comment.id not in already_done:
