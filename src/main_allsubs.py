576bf64195a81e69c3e3d32b6430b2e29be0140c
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-01-03 18:59:44 +0200

    Fixed bugs

--- src/main_allsubs.py
+++ src/main_allsubs.py
@@ -1,16 +1,17 @@
 import wdbm
-import main
+import main as m
 
 def main():
     """Recursively run reddit comment parser for top 1000 posts of all time on
     every subreddit in subreddits.txt"""
     subs = []
     with open("subreddits.txt", "r") as f:
-        for line in f:
-            subs.append("".join(wdbm.filtrate(line))[1:])
+        for sub in f:
+            subs.append(sub[:-1])
 
     for subreddit in subs:
-        mgr = main.RedditWDBMS(subreddit, 1000)
+        print subreddit
+        mgr = m.RedditWDBM(subreddit, 1000)
         mgr.main()
 
 if __name__ == "__main__":
